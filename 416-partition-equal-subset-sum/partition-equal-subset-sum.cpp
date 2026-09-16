49595636b621efc1b9b7f9a70607a03b08bef31b
Shivamyadav-4128 <yshivam26711@gmail.com>
2026-09-16 21:04:05 +0530

    Time: 173 ms (22.56%) | Memory: 17.5 MB (56.98%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,34 +1,34 @@
 
 class Solution {
 public:
-   int dp[201][10001];
-    bool fxn(vector<int>& arr, int i, int cs, int ts) {
-        if (cs == ts / 2) return true;
-        if (i < 0 || cs > ts / 2) return false;
-
-        if(dp[i][cs] != -1)
-          return dp[i][cs];
-
-        // Ex
-        bool ex = fxn(arr, i - 1, cs, ts);
-
-        // In
-        bool in = false;
-        if(cs + arr[i] <= ts / 2)
-          in = fxn(arr, i - 1, cs + arr[i], ts);
-
-        return dp[i][cs] =  in || ex;
-    }
-
     bool canPartition(vector<int>& nums) {
+        int n = nums.size();
         int ts = 0;
 
         for (int x : nums)
             ts += x;
 
         if (ts % 2 != 0) return false;
-        memset(dp, -1, sizeof(dp));
 
-        return fxn(nums, nums.size() - 1, 0, ts);
+        int target = ts / 2;
+
+        vector<vector<bool>> dp(n + 1, vector<bool>(target + 1, false));
+
+        for(int i = 0; i <= n; i++)   //bc
+            dp[i][0] = true;
+
+            for(int i = 1; i <= n; i++){
+                for(int cs = 1; cs <= target; cs++){
+
+                    bool ex = dp[i - 1][cs];
+
+                    bool in = false;
+                    if(nums[i - 1] <= cs)
+                      in = dp[i - 1][cs - nums[i - 1]];
+
+                      dp[i][cs] = in || ex;
+                }
+            }
+            return dp[n][target];
     }
 };
