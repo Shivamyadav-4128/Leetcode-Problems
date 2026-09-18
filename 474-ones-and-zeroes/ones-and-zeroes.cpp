688844bad3a12d1362f19edb5280ee713bbbfca3
Shivamyadav-4128 <yshivam26711@gmail.com>
2026-09-18 14:33:53 +0530

    Time: 41 ms (94.75%) | Memory: 13.8 MB (74.23%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/ones-and-zeroes.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-    int dp[601][101][101];
-    int solve(int i, int m, int n, vector<string>& strs) {
-        if(i == strs.size())
-          return 0;
-
-          if(dp[i][m][n] != -1)
-            return dp[i][m][n];
-          int z = 0, o = 0;
-          for(char c : strs[i]) {
-            if(c == '0') z++;
-            else o++;
-          }
-          int ex = solve(i + 1, m, n, strs);
-          int in = 0;
-          if(z <= m && o <= n)
-            in = 1 + solve(i + 1, m - z, n - o, strs);
-
-            return dp[i][m][n] =  max(in, ex);
-    }
     int findMaxForm(vector<string>& strs, int m, int n) {
-        memset(dp, -1, sizeof(dp));
-        return solve(0, m, n, strs);
+        vector<vector<int>> dp(m +1, vector<int>(n +1, 0));
+
+        for(string s : strs) {
+            int z = 0, o = 0;
+
+            for(char c : s){
+                if(c == '0') z++;
+                else o++;
+            }
+            for(int i = m; i >= z; i--){
+                for(int j = n; j >= o; j--){
+                    dp[i][j] = max(dp[i][j], 1 + dp[i - z][j - o]);
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
